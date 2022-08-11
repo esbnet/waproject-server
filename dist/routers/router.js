12b23b7bf74c74580fab80683d40f20e3292db03
Edmilson Soares <esbnet@gmail.com>
2022-08-11 20:07:48 -0300

    feat: :wrench: config cors

--- dist/routers/router.js
+++ dist/routers/router.js
@@ -2,23 +2,23 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.Router = void 0;
-var express_1 = __importDefault(require("express"));
-var MovieController_1 = __importDefault(require("../controllers/MovieController"));
-var swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
-var Router = express_1["default"].Router();
+const express_1 = __importDefault(require("express"));
+const MovieController_1 = __importDefault(require("../controllers/MovieController"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const Router = express_1.default.Router();
 exports.Router = Router;
-var swaggerDocs = require("../swagger.json");
-Router.get("/", function (req, res) {
+const swaggerDocs = require("../swagger.json");
+Router.get("/", (req, res) => {
     return res.send("<h1>Welcome API WAproject!</>");
 });
-Router.use("/api-docs", swagger_ui_express_1["default"].serve, swagger_ui_express_1["default"].setup(swaggerDocs));
-Router.get("/terms", function (req, res) {
+Router.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDocs));
+Router.get("/terms", (req, res) => {
     return res.json({ message: "Terms and Conditions" });
 });
-Router.get("/movies", MovieController_1["default"].findMovies);
-Router.get("/movies/count", MovieController_1["default"].countMovies);
-Router.get("/movies/load", MovieController_1["default"].createManyMovies);
-Router.get("/movies/reset", MovieController_1["default"].resetMovies);
+Router.get("/movies", MovieController_1.default.findMovies);
+Router.get("/movies/count", MovieController_1.default.countMovies);
+Router.get("/movies/load", MovieController_1.default.createManyMovies);
+Router.get("/movies/reset", MovieController_1.default.resetMovies);
 //# sourceMappingURL=router.js.map
--- dist/server.js
+++ dist/server.js
@@ -2,28 +2,28 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-exports.__esModule = true;
-var express_1 = __importDefault(require("express"));
-var cors_1 = __importDefault(require("cors"));
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 require("express-async-errors");
-var router_1 = require("./routers/router");
-var port = process.env.PORT || 9000;
-var app = (0, express_1["default"])();
-app.use((0, cors_1["default"])());
-app.use(express_1["default"].json());
+const router_1 = require("./routers/router");
+const port = process.env.PORT || 9000;
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
 app.use("/v1", router_1.Router);
-app.use(function (err, request, response, next) {
+app.use((err, request, response, next) => {
     if (err instanceof Error) {
         return response.status(400).json({
-            message: err.message
+            message: err.message,
         });
     }
     return response.status(500).json({
         status: "error",
-        message: "Internal server error - ".concat(err)
+        message: `Internal server error - ${err}`,
     });
 });
-app.listen(port, function () {
-    console.log("\uD83D\uDE80 Server ready at: http://localhost:".concat(port));
+app.listen(port, () => {
+    console.log(`🚀 Server ready at: http://localhost:${port}`);
 });
 //# sourceMappingURL=server.js.map
