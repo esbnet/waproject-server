a7d13874968df48683773a4229c6d456b41dda5f
Edmilson Soares <esbnet@gmail.com>
2022-08-11 20:34:17 -0300

    config cors

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -6,7 +6,7 @@ const Router = express.Router();
 const swaggerDocs = require("../swagger.json");
 
 Router.get("/", (req: Request, res: Response) => {
-	return res.send("<h1>Welcome API WAproject!</><span>Versão: 1.0</span> ");
+	return res.send("<h1>Welcome API WAproject!</></br><span>Versão: 1.0</span> ");
 });
 Router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 Router.get("/terms", (req, res) => {
